457523f9ba76da411084fa32f14108e54f8c965f
William <williamrezendee@gmail.com>
2022-02-21 14:55:46 -0300

    Adicionando as demos das aulas 03 e 04

---
+++ Trabalhando-com-tipos-referencia-e-valor/Aula-03/Demo01/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "William";
+p1.Idade = 30;
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua teste",
+    Numero = 300,
+    CEP = "000000",
+    Cidade = "Itabaiana"
+};
+
+WriteLine("FIM");
