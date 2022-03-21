f116e1146375f08c63c0c1df7ab93eadd4a82a56
William Rezende <williamrezendee@gmail.com>
2022-03-21 19:30:03 -0300

    Adicionando as modificações finais nos arquivos do bootcamp

--- desafios-finais/ComprasNoSupermercado/Program.cs
+++ desafios-finais/ComprasNoSupermercado/Program.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 class MinhaClasse {
   public static void Main (string[] args) {
     var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
     for (int i = 0; i < totalDeCasosDeTeste; i++)
     {
-        
+        string[] itens = Console.ReadLine().Split(' ');
+        var set = CriarConjuntoNaoRepetido(itens);
+        Console.WriteLine(string.Join(" ",set));
     }
+  }
+  static SortedSet<string> CriarConjuntoNaoRepetido(string[] itens)
+  {
+    var sortedSet = new SortedSet<string>();
+    foreach (var item in itens)
+    {
+      sortedSet.Add(item);
+    }
+    return sortedSet;
   }
 }
